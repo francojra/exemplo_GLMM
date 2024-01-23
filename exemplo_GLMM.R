c76a89653eeab7ccd566197479f666e42f2a533f
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-01-22 23:53:30 -0300

    Add files via upload

--- exemplo_GLMM.R
+++ exemplo_GLMM.R
@@ -19,3 +19,34 @@
 ### Os efeitos aleatórios são usados para modelar as correlações entre as 
 ### observações dentro dos grupos, permitindo uma consideração mais realista 
 ### da estrutura dos dados.
+
+# Análises ---------------------------------------------------------------------------------------------------------------------------------
+
+### Para ajustar uma análise GLMM, necessitamos utilizar o pacote 'lme4'.
+
+# Instale o pacote se ainda não estiver instalado
+# install.packages("lme4")
+
+# Carregue o pacote
+
+library(lme4)
+
+# Gere dados de exemplo
+
+set.seed(123)
+n_individuals <- 50
+n_measurements <- 5
+
+data <- expand.grid(Individual = 1:n_individuals, Time = 1:n_measurements)
+
+View(data)
+
+data$Outcome <- rbinom(n_individuals * n_measurements, size = 1, prob = 0.3)
+data$Covariate <- rnorm(n_individuals * n_measurements)
+
+# Ajuste do GLMM
+glmm_model <- glmer(Outcome ~ Covariate + (1|Individual), data = data, family = binomial)
+
+# Visualize os resultados
+summary(glmm_model)
+
